eeebc139e3373d95095fb66c07e580ada0786af6
Alejandro Carrillo <149031448+anga-carq23@users.noreply.github.com>
2024-12-01 20:44:57 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Controlar la música de fondo
+function toggleMusic() {
+    const music = document.getElementById('background-music');
+    if (music.paused) {
+        music.play();
+    } else {
+        music.pause();
+    }
+}
